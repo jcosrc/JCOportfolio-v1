93bd1c63dd9278443b7ba215ea3abfe1c77458d0
JCO <jcosrcdev@gmail.com>
2025-08-27 19:05:36 +0800

    asdf

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/JCOportfolio-v1/',
+  base: '/',
   plugins: [
     tailwindcss(),
     react(),
